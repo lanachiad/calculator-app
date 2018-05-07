c85cec8a248761f9a913378c5bbc5d55dff756b8
lanachiad <lanachiad7@gmail.com>
2018-05-07 13:30:22 -0400

    Add test content

---
+++ Field.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+class Field extends React.Component {
+	render() {
+		return(
+			<View>
+				<Text>Here's the input field</Text>
+			</View>
+		)
+	}
+}
+
+export default Field;
