ed680fd8b79c8ba687d789df6ea5abd386f9caa1
lanachiad <lanachiad7@gmail.com>
2018-05-07 12:04:09 -0400

    Add placeholder styling

--- Calculator.js
+++ Calculator.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 class Calculator extends React.Component {
 	render() {
 		return (
-			<View>
+			<View style={styles.container}>
 				<Text>Hi, this is a calculator</Text>
 			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		flex: 1,
+		justifyContent: 'center'
+	}
+})
 
 export default Calculator;
