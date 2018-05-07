1d3aac5a45b1d28dc944526cf33d9fd29b071f8d
lanachiad <lanachiad7@gmail.com>
2018-05-07 12:19:47 -0400

    Render buttons dynamically

--- Calculator.js
+++ Calculator.js
@@ -6,8 +6,16 @@ class Calculator extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Hi, this is a calculator</Text>
-				<Button />
+				<Button value={1} />
+				<Button value={2} />
+				<Button value={3} />
+				<Button value={4} />
+				<Button value={5} />
+				<Button value={6} />
+				<Button value={7} />
+				<Button value={8} />
+				<Button value={9} />
+				<Button value={0} />
 			</View>
 		)
 	}
