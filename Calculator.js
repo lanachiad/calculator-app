b741c4863a959ef96cb108d84068106ee4d9515d
lanachiad <lanachiad7@gmail.com>
2018-05-07 11:42:48 -0400

    Test content

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class Calculator extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>Hi, this is a calculator</Text>
+			</View>
+		)
+	}
+}
+
+export default Calculator;
