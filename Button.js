404ec1fcd10cfae5083ddf2dc829c024b36f3aec
lanachiad <lanachiad7@gmail.com>
2018-05-07 12:06:00 -0400

    Test content

---
+++ Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, TouchableHighlight, View} from 'react-native';
+
+class Button extends React.Component {
+	render() {
+		return (<Text>test</Text>)
+	}
+}
+
+export default Button;
