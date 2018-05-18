27fb4096eb1e3000d3cf0e878de1dfb9d643efcc
Lana Chiad <lanachiad7@gmail.com>
2018-05-18 14:09:26 -0400

    Fixed setState fn

--- Calculator.js
+++ Calculator.js
@@ -4,22 +4,21 @@ import Button from './Button';
 import { Alert, StyleSheet, Text, View } from 'react-native';
 
 class Calculator extends React.Component {
-	constructor(props) {
-		super(props);
+	constructor() {
+		super();
 		this.state = {
 			currentInput: 0
-		}
+		};
 	}
 
-	onBtnPress = (newInput) => {
-		this.setState = ({ currentInput: newInput })
-		// this.displayInput();
-	}
+	onBtnPress = newInput => {
+		this.setState({ currentInput: newInput });
+	};
 
-		displayInput = () => {
-			let currentDisplay = this.state.currentInput;
-			return currentDisplay;
-		}
+	displayInput = () => {
+		let currentDisplay = this.state.currentInput;
+		return currentDisplay;
+	};
 
 	render() {
 		let currentDisplay = this.state.currentInput;
@@ -28,36 +27,36 @@ class Calculator extends React.Component {
 			<View style={styles.btnContainer}>
 				<Field input={currentDisplay} />
 				<View style={styles.btnWrapper}>
-					<Button triggerButton={this.onBtnPress(1)} value={1} />
-					<Button triggerButton={this.onBtnPress(2)} value={2} />
-					<Button triggerButton={this.onBtnPress(3)} value={3} />
+					<Button triggerButton={this.onBtnPress} value={1} />
+					<Button triggerButton={this.onBtnPress} value={2} />
+					<Button triggerButton={this.onBtnPress} value={3} />
 				</View>
 				<View style={styles.btnWrapper}>
-					<Button value={4} />
-					<Button value={5} />
-					<Button value={6} />
+					<Button triggerButton={this.onBtnPress} value={4} />
+					<Button triggerButton={this.onBtnPress} value={5} />
+					<Button triggerButton={this.onBtnPress} value={6} />
 				</View>
 				<View style={styles.btnWrapper}>
-					<Button value={7} />
-					<Button value={8} />
-					<Button value={9} />
+					<Button triggerButton={this.onBtnPress} value={7} />
+					<Button triggerButton={this.onBtnPress} value={8} />
+					<Button triggerButton={this.onBtnPress} value={9} />
 				</View>
 				<View style={styles.btnWrapper}>
-					<Button value={0} />
+					<Button triggerButton={this.onBtnPress} value={0} />
 				</View>
 			</View>
-		)
+		);
 	}
 }
 
 const styles = StyleSheet.create({
 	btnContainer: {
 		alignItems: 'center',
-		marginTop: 100,
+		marginTop: 100
 	},
 	btnWrapper: {
-		flexDirection: 'row',
+		flexDirection: 'row'
 	}
-})
+});
 
 export default Calculator;
